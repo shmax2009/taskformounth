3685462afd08673493d283a6238f21f75e4cc82f
Shmax <maximschved8@gmail.com>
2020-11-21 12:31:15 +0200

    2day

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 enum Superposition {
     Null, Right /* '--- / ---' */, Left /* '--- \ ---' */
 };
@@ -20,6 +19,7 @@ public:
 
     friend istream &operator>>(istream &in, Point &point) {
         in >> point.x >> point.y;
+        return in;
     };
 };
 
@@ -40,34 +40,119 @@ public:
 
     friend istream &operator>>(istream &in, Triangle &triangle) {
         in >> triangle.point1 >> triangle.point2 >> triangle.point3;
+        return in;
+
     };
 };
-int length(Point point1, Point point2) {
-    return sqrt(abs(point1.x - point2.x) * abs(point1.x - point2.x) + abs(point1.y - point2.y) * abs(point1.y - point2.y));
-}
 
 typedef vector<vector<Square>> Field;
 
-vector<vector<Square>> initMatrix(int n, int m) {
+Field initMatrix(int n, int m) {
     Field matrix;
-    for (int x = 0; x < n; x++) {
-        vector<Square> column;
-        for (int y = 0; y < m; y++) {
+    for (int y = 0; y < n; y++) {
+        vector<Square> row;
+        for (int x = 0; x < m; x++) {
             Square square;
             square.position.x = x;
             square.position.y = y;
             square.rotate = Null;
-            column.push_back(square);
+            row.push_back(square);
         }
-        matrix.push_back(column);
+        matrix.push_back(row);
     }
 
     return matrix;
 }
 
+double crossProduct(Point a, Point b) {
+    return (double)(a.x) * b.y - (double)(a.y) * b.x;
+}
 
-void convertTriangleToSquare(Triangle triangle) {
-    //TODO
+Point makeVector(Point a, Point b) {
+    return { b.x - a.x, b.y - a.y };
+}
+
+bool isPointInTriangle(Triangle triangle, Point point) {
+    Point a = triangle.point1;
+    Point b = triangle.point2;
+    Point c = triangle.point3;
+
+    Point ab = makeVector(a, b);
+    Point bc = makeVector(b, c);
+    Point ca = makeVector(c, a);
+
+    Point ap = makeVector(a, point);
+    Point bp = makeVector(b, point);
+    Point cp = makeVector(c, point);
+
+    double abp = crossProduct(ab, ap);
+    double bcp = crossProduct(bc, bp);
+    double cap = crossProduct(ca, cp);
+
+    return (abp <= 0 && bcp <= 0 && cap <= 0) || (abp >= 0 && bcp >= 0 && cap >= 0);
+}
+
+void convertTriangleToSquare(Field &field, Triangle triangle) {
+    for (int y = 0; y < field.size(); y++) {
+        for (int x = 0; x < field[y].size(); x++) {
+            Point LowLeftPoint;
+            Point LowRightPoint;
+            Point HeightLeftPoint;
+            Point HeightRightPoint;
+
+            // находится ли точка в треугольнике
+            bool LowLeft;
+            bool LowRight;
+            bool HeightLeft;
+            bool HeightRight;
+
+            LowLeftPoint = {x, y};
+            LowRightPoint = {x + 1, y};
+            HeightLeftPoint = {x, y + 1};
+            HeightRightPoint = {x + 1, y + 1};
+
+            LowLeft = isPointInTriangle(triangle, LowLeftPoint);
+            LowRight = isPointInTriangle(triangle, LowRightPoint);
+            HeightLeft = isPointInTriangle(triangle, HeightLeftPoint);
+            HeightRight = isPointInTriangle(triangle, HeightRightPoint);
+            cout << x << " " << y << endl;
+            cout << LowLeft << " " << LowRight << " " << HeightLeft << " " << HeightRight << endl;
+            // если точек входит в треугольник <= 2, тогда выходим
+            if (((int) LowLeft + (int) LowRight + (int) HeightLeft + (int) HeightRight) <= 2)
+                continue;
+
+            // иначе, то-есть >= 3 входят
+            if (LowLeft and LowRight and HeightLeft and HeightRight) {
+                field[x][y].rotate = Null;
+                field[x][y].lowerTriangle = Red;
+                field[x][y].upperTriangle = Red;
+                continue;
+            }
+            if (!HeightLeft) {
+                field[x][y].rotate = Right;
+                field[x][y].lowerTriangle = Red;
+                field[x][y].upperTriangle = Blue;
+            }
+            if (!HeightRight) {
+                field[x][y].rotate = Left;
+                field[x][y].lowerTriangle = Red;
+                field[x][y].upperTriangle = Blue;
+            }
+
+            if (!LowLeft) {
+                field[x][y].rotate = Right;
+                field[x][y].lowerTriangle = Blue;
+                field[x][y].upperTriangle = Red;
+            }
+            if (!LowRight) {
+                field[x][y].rotate = Left;
+                field[x][y].lowerTriangle = Blue;
+                field[x][y].upperTriangle = Red;
+            }
+
+
+        }
+    }
 }
 
 bool checkSquare(int a) {
@@ -84,6 +169,35 @@ int main() {
     for (int i = 0; i < trianglesCount; i++) {
         Triangle triangle;
         cin >> triangle;
+        convertTriangleToSquare(field, triangle);
     }
+
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < m; x++) {
+            cout << x << " " << y << endl;
+            if (field[x][y].rotate == Left) {
+                cout << "left" << endl;
+            } else if (field[x][y].rotate == Right) {
+                cout << "right" << endl;
+
+            }
+            if (field[x][y].upperTriangle == Red) {
+                cout << "Red" << endl;
+            } else {
+                cout << "Blue" << endl;
+            }
+
+            if (field[x][y].lowerTriangle == Red) {
+                cout << "Red" << endl;
+            } else {
+                cout << "Blue" << endl;
+
+            }
+
+        }
+    }
+
 }
 // 271 - M1, 500 - M2
+
+//https://github.com/shmax2009/taskformounth
--- testFile.cpp
+++ testFile.cpp
@@ -2,6 +2,76 @@
 
 using namespace std;
 
-int main() {
+class Point {
+public:
+    int x;
+    int y;
 
+    friend istream &operator>>(istream &in, Point &point) {
+        in >> point.x >> point.y;
+        return in;
+    };
+};
+
+class Triangle {
+public:
+    Point point1;
+    Point point2;
+    Point point3;
+
+    friend istream &operator>>(istream &in, Triangle &triangle) {
+        in >> triangle.point1 >> triangle.point2 >> triangle.point3;
+        return in;
+
+    };
+};
+
+bool res(Triangle triangle, Point point) {
+    double xa = triangle.point1.x;
+    double ya = triangle.point1.y;
+    double xb = triangle.point2.x;
+    double yb = triangle.point2.y;
+    double xc = triangle.point3.x;
+    double yc = triangle.point3.y;
+    double yd = point.x;
+    double xd = point.x;
+    return (((((xd - xa) * (yb - ya) - (yd - ya) * (xb - xa)) * ((xc - xa) * (yb - ya) - (yc - ya) * (xb - xa)) >= 0) &&
+            (((xd - xb) * (yc - yb) - (yd - yb) * (xc - xb)) * ((xa - xb) * (yc - yb) - (ya - yb) * (xc - xb)) >= 0) &&
+            (((xd - xc) * (ya - yc) - (yd - yc) * (xa - xc)) * ((xb - xc) * (ya - yc) - (yb - yc) * (xa - xc)) >= 0)) ||
+            ((((xd - xa) * (yb - ya) - (yd - ya) * (xb - xa)) * ((xc - xa) * (yb - ya) - (yc - ya) * (xb - xa)) <= 0) &&
+            (((xd - xb) * (yc - yb) - (yd - yb) * (xc - xb)) * ((xa - xb) * (yc - yb) - (ya - yb) * (xc - xb)) <= 0) &&
+            (((xd - xc) * (ya - yc) - (yd - yc) * (xa - xc)) * ((xb - xc) * (ya - yc) - (yb - yc) * (xa - xc)) <= 0)));
+}
+
+bool res1(Triangle triangle, Point point) {
+    Point ab = makeVector(triangle.point1, triangle.point2);
+    Point bc = makeVector(triangle.point2, triangle.point3);
+    Point ca = makeVector(triangle.point3, triangle.point1);
+
+    Point ap = makeVector(a, point);
+    Point bp = makeVector(b, point);
+    Point cp = makeVector(c, point);
+
+    Point abp = crossProduct(ab, ap);
+    Point bcp = crossProduct(bc, bp);
+    Point cap = crossProduct(ca, cp);
+
+    return (abp <= 0 && bcp <= 0 && cap <= 0) || (abp >= 0 && bcp >= 0 && cap >= 0);
+}
+
+Point crossProduct(Point a, Point b) {
+    return a.x * b.y -  a.y * b.x;
+}
+
+Point makeVector(Point a, Point b) {
+    return new Point(b.x - a.x, b.y - a.y);
+}
+
+int main() {
+    Point point1;
+    Point point2;
+    Point point3;
+    Point selfPoint;
+    cin >> point1 >> point2 >> point3 >> selfPoint;
+    cout << res1({point1, point2, point3}, selfPoint);
 }
